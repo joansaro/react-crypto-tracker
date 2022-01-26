46c2ef214135537722ae51f5ae8e919fb3254e1a
joansaro <joansaro.sant@gmail.com>
2022-01-25 19:46:46 -0600

    Responsive desing

--- src/Coin.js
+++ src/Coin.js
@@ -10,7 +10,7 @@ const Coin = ({name, image, symbol, price, volume, priceChange, marketcap}) => {
                     <p className='coin-symbol'>{symbol}</p>
                 </div>
                 <div className='coin-data'>
-                    <p className='coin-price'>{price}</p>
+                    <p className='coin-price'>${price}</p>
                     <p className='coin-volume'>${volume.toLocaleString()}</p>
                     {priceChange < 0 ? (
                         <p className="coin-percent red">{priceChange.toFixed(2)}%</p>
